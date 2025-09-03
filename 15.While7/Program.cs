3e8b5d782c3ab7be06e398fb36048f4e0eb8a16b
LauraArdila08 <laura.ardilaa@upb.edu.co>
2025-09-03 15:33:22 -0500

    15. While ejercicio 7

---
+++ 15.While7/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _15.While7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*15. Realizar un algoritmo para determinar cuánto ahorrará una persona en un año, si al final de cada mes deposita cantidades variables de dinero;
+            además, se quiere saber cuánto lleva ahorrado cada mes. */
+
+            int ahorro = 0;
+
+
+            Console.WriteLine("");
+
+        }
+
+    }
+}
