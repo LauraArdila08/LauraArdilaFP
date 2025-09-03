66ba21e93b1a20b9dfa1889e880a02acc67da775
LauraArdila08 <laura.ardilaa@upb.edu.co>
2025-09-03 15:32:11 -0500

    13.While ejercicio 5

---
+++ 13.While5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _13.While5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Escribir un algoritmo que cuente cuántos números pares hay en un rango de números del 1 al número n
+
+            int contador = 1;
+            int cantidadPares = 0;
+            int rango = 0;
+
+            Console.WriteLine("Ingrese el rango de números a evaluar");
+            rango = Int32.Parse(Console.ReadLine());
+
+            while (contador <= rango)
+            {
+                if (contador %2==0)
+                {
+                    cantidadPares++;
+                }
+                contador++;
+            }
+            Console.WriteLine( $"La cantidad de números pares del {rango}, es {CantidadPares}"
+        }
+    }
+}
