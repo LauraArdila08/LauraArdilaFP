623948e2a6d9a24748cc30ad362c8ffecd44f299
LauraArdila08 <laura.ardilaa@upb.edu.co>
2025-09-03 15:32:52 -0500

    14. While ejercicio 6

---
+++ 14.While6/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _14.While6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*14. Realizar un algoritmo que le pida al usuario ingresar por teclado números enteros al azar. Al usuario le corresponde ingresar la cantidad de números que va a introducir.  
+            El algoritmo debe escribir en pantalla: 
+            La cantidad de números introducidos que son mayores que 0 
+            La cantidad de números introducidos menores que 0  
+            La cantidad de números iguales a 0.*/
+
+            int cantidadNumeros = 0;
+
+            int contador = 1;
+            int mayoresCero = 0;
+            int menoresCeros = 0;
+            int numIgualCero = 0;
+
+            Console.WriteLine("Cantidad de números a ingresar");
+            cantidadNumeros = Int32.Parse(Console.ReadLine());
+
+            while()
+            {
+
+            }
+
+
+
+        }
+    }
+}
