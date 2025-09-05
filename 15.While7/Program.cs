dea1c41210f24adb7109e916248e0633e3160172
LauraArdila08 <laura.ardilaa@upb.edu.co>
2025-09-05 11:25:08 -0500

    15. While 7 ejercicio completo

--- 15.While7/Program.cs
+++ 15.While7/Program.cs
@@ -7,11 +7,22 @@
             /*15. Realizar un algoritmo para determinar cuánto ahorrará una persona en un año, si al final de cada mes deposita cantidades variables de dinero;
             además, se quiere saber cuánto lleva ahorrado cada mes. */
 
-            int ahorro = 0;
+            int mes = 1;
+            int ahorroTotal = 0;
+            int deposito = 0;
 
+            while(mes <= 12)
+            {
+                Console.WriteLine($"¿Cuánto va a depositar en el mes {mes}?");
+                deposito = Int32.Parse( Console.ReadLine() );
 
-            Console.WriteLine("");
+                ahorroTotal = ahorroTotal + deposito;
 
+                Console.WriteLine($"Después del mes {mes} lleva ahorrado {ahorroTotal}");
+                mes++;
+            }
+
+            Console.WriteLine($"El ahorro total al final del año es: {ahorroTotal}");
         }
 
     }
