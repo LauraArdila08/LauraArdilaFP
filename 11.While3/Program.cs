d5b555f4231f1224a3f39918522cd6862cd910da
LauraArdila08 <laura.ardilaa@upb.edu.co>
2025-09-05 13:15:14 -0500

    11. While 3 ejercicio resuelto

---
+++ 11.While3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _11.While3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*11. Realizar un algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo. 
+            Se debe mostrar por pantalla el total de la suma de los números ingresados*/
+
+            int numero = 0;
+            int suma = 0;
+            
+
+            while (0 <= numero)
+            {
+                Console.WriteLine("Ingrese un número:");
+                numero = Int32.Parse(Console.ReadLine());
+
+                suma = suma + numero;
+            }
+
+            Console.WriteLine($"La suma total es: {suma}");
+        }
+    }
+}
