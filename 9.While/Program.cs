767480306e211cda28990c607fa41e0c8307c549
LauraArdila08 <laura.ardilaa@upb.edu.co>
2025-09-09 18:01:06 -0500

    9. While, contador y acumulador

---
+++ 9.While/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _9.While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //9.Escribir un algoritmo que permita generar primeros 5 números enteros positivos y realizar y mostrar su suma.
+
+            int contador = 0;
+            int acumulador = 0;
+
+            while (contador <= 5) 
+            { 
+                acumulador = acumulador + contador;
+                contador++;
+            }
+            Console.WriteLine($"La suma de los 5 primeros números es {acumulador}");
+        }
+    }
+}
